092c1d0b33bb508f00afedaedc472d16b62b22d9
yohanis-ke <yohanes.ke@gmail.com>
2018-09-15 12:44:16 -0400

    the person class and the greeting method is created

---
+++ people.rb
@@ -0,0 +1,21 @@
+class Person
+  def initialize(name)
+    @name = name
+
+  end
+  def greeting
+    puts " Hi my name is #{name}"
+  end
+end
+
+class Student
+  def learn
+    puts "I get it"
+  end
+end
+
+class Instructor
+  def teach
+    puts "everything in ruby is an object"
+  end
+end
